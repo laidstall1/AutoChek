383123c40c85bef8bf5a046316f81bcacbbb5233
Oluwatoni <laidstall@gmail.com>
2021-08-08 03:10:40 +0100

    add Collection view delegate and datasource

--- AutoChek/AutoChek/Controllers/RootViewController.swift
+++ AutoChek/AutoChek/Controllers/RootViewController.swift
@@ -10,15 +10,43 @@ import UIKit
 class RootViewController: UIViewController {
 
     //  MARK: - Properties
-    @IBOutlet weak var tableView: UITableView!
+    
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var carCategoriesCollectionView: UICollectionView!
+    @IBOutlet weak var carListingCollectionView: UICollectionView!
     
     //  MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionViewSetup()
     }
+    
     //  MARK: - Helpers
-
+    
+    func collectionViewSetup() {
+        carCategoriesCollectionView.delegate = self
+        carCategoriesCollectionView.dataSource = self
+        carListingCollectionView.delegate = self
+        carListingCollectionView.dataSource = self
+    }
 }
 
+    //  MARK: - UITableViewDataSource
+
+extension RootViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
+
+//  MARK: - UITableViewDelegate
+
+extension RootViewController: UICollectionViewDelegate {
+    
+}
