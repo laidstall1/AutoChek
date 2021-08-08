742b061d90fa50295b6ca6996f1ee716beec2813
Oluwatoni <laidstall@gmail.com>
2021-08-08 02:25:27 +0100

    add project setup and storyboards.

---
+++ AutoChek/AutoChek/Controllers/RootViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AutoChek
+//
+//  Created by mac on 07/08/2021.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    //  MARK: - Properties
+    
+    //  MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    //  MARK: - Helpers
+
+}
+
