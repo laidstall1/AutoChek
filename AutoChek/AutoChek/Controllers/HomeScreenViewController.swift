683c8fa0952b5eeb58c053c4fca158ac631a5e39
Oluwatoni <laidstall@gmail.com>
2021-08-09 05:54:09 +0100

    will create a configure function on the CarCategoryCell to populate the carMakeImageView and CarMakeName, call fetchCarMake function from the viewmodel on the HomeScreenViewController and change from dummy data

--- AutoChek/AutoChek/Controllers/HomeScreenViewController.swift
+++ AutoChek/AutoChek/Controllers/HomeScreenViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class HomeScreenViewController: UIViewController {
 
     //  MARK: - Properties
+    private let viewModel = HomeScreenViewModel()
     @IBOutlet weak var carCategoriesCollectionView: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var carListingCollectionView: UICollectionView!
@@ -20,14 +21,24 @@ class HomeScreenViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9685223699, green: 0.9686879516, blue: 0.9685119987, alpha: 1)
         collectionViewSetup()
+        fetchCarMake()
     }
+
     
     //  MARK: - Helpers
+    
+    func fetchCarMake() {
+        self.viewModel.fetchCarMake {
+            DispatchQueue.main.async {
+                self.carCategoriesCollectionView.reloadData()
+            }
+        }
+    }
     
     func collectionViewSetup() {
         carCategoriesCollectionView.delegate = self
         carCategoriesCollectionView.dataSource = self
-        carListingCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
+//        carListingCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
         carListingCollectionView.delegate = self
         carListingCollectionView.dataSource = self
         carListingCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
@@ -40,13 +51,15 @@ class HomeScreenViewController: UIViewController {
 
 extension HomeScreenViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return collectionView == carCategoriesCollectionView ? 10 : 20
+        print(viewModel.data.count)
+        return collectionView == carCategoriesCollectionView ? viewModel.data.count : 20
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == carCategoriesCollectionView {
             let cell = carCategoriesCollectionView.dequeueReusableCell(withReuseIdentifier: CarCategoryCell.identifier, for: indexPath) as! CarCategoryCell
+            cell.configure(with: viewModel.data[indexPath.row])
             return cell
         } else {
             let cell = carListingCollectionView.dequeueReusableCell(withReuseIdentifier: CarListingCell.identifier, for: indexPath) as! CarListingCell
@@ -60,7 +73,6 @@ extension HomeScreenViewController: UICollectionViewDataSource {
 
 extension HomeScreenViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        print(view.frame.width-136)
         return CGSize(width: view.frame.width-136, height: 330)
     }
     
@@ -72,4 +84,3 @@ extension HomeScreenViewController: UICollectionViewDelegateFlowLayout {
         }
     }
 }
-
